7d53a67a0dfcd017ec20cf3687c6e3a7ead23310
Herobroto <herobrotelpro@gmail.com>
2023-06-19 11:47:26 -0600

    acomodar Login

--- src/componentsUser/Vistas/Login.jsx
+++ src/componentsUser/Vistas/Login.jsx
@@ -18,7 +18,7 @@ function BasicExample() {
       <Navbar></Navbar>
       </header>
       <body>
-        <Form >
+        <Form className='login'>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Ingresa tu correo ó usuario</Form.Label>
             <Form.Control 
