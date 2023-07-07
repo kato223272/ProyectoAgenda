e035ab518d2bb6c0ce0bafa381f6ea266d8ea739
kato223272 <223272@ids.upchiapas.edu.mx>
2023-07-07 01:29:58 -0600

    actualizacion2

---
+++ src/componentsUser/Vistas/Historial.jsx
@@ -0,0 +1,19 @@
+import Navbar from "../Componentes/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Inicio() {
+  return (
+    <form action="">
+      <header>
+        <Navbar></Navbar>
+      </header>
+    <body>
+    <br />
+    </body>
+     <footer>
+        
+     </footer>
+    </form>
+  );
+}
+export default Inicio;
