6b594e1844dc9a2c90b11e784d8ac2723dc829f5
kato223272 <223272@ids.upchiapas.edu.mx>
2023-07-16 20:05:59 -0600

    actualizacion pre-visual

--- src/componentsUser/Componentes/CardFavoritos.jsx
+++ src/componentsUser/Componentes/CardFavoritos.jsx
@@ -10,25 +10,42 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 function Inicio() {
-  return (
-    <form action="">
-      <div>
-        <Row>
-          <Col>
+  // Supongamos que la información de la base de datos se encuentra en un array llamado "datos" y si cambiala Jeshua
+  const datos = [
+    {
+      nombreEmpresa: 'Empresa 1',
+      biografia: 'Biografía de la Empresa 1',
+      telefono: '1234567890',
+      citaUrl: '/AgendarCitaEmpresa1',
+    },
+    // Agregar más objetos con la información de las demás empresas
+  ];
+
+  // Función para crear las cards dinámicamente
+  const renderCards = () => {
+    const cards = [];
+
+    for (let i = 0; i < datos.length; i += 4) {
+      // Crear una fila para cada grupo de 4 cards
+      const filaCards = [];
+      for (let j = i; j < i + 4 && j < datos.length; j++) {
+        const empresa = datos[j];
+        filaCards.push(
+          <Col key={j}>
             <div className='cardDiv' style={{ padding: '5%', textAlign: 'center' }}>
               <Card style={{ width: '25%' }}>
                 <Card.Img variant="top" src={imgg} />
                 <Card.Body>
-                  <Card.Title>Nombre de la empresa</Card.Title>
+                  <Card.Title>{empresa.nombreEmpresa}</Card.Title>
                   <Card.Text>
-                    <FontAwesomeIcon icon={faBook} style={{ marginRight: '5px' }} /> Biografía
+                    <FontAwesomeIcon icon={faBook} style={{ marginRight: '5px' }} /> {empresa.biografia}
                   </Card.Text>
                   <Card.Text>
-                    <FontAwesomeIcon icon={faPhone} style={{ marginRight: '5px' }} /> Número de teléfono
+                    <FontAwesomeIcon icon={faPhone} style={{ marginRight: '5px' }} /> {empresa.telefono}
                   </Card.Text>
                   <div className="d-flex justify-content-center">
-                    <Button className='botonAgendar' variant="primary" href='/AgendarCita'>
-                      <FontAwesomeIcon  icon={faCalendarAlt} style={{ marginRight: '5px' }} />
+                    <Button className='botonAgendar' variant="primary" href={empresa.citaUrl}>
+                      <FontAwesomeIcon icon={faCalendarAlt} style={{ marginRight: '5px' }} />
                       Agendar cita
                     </Button>
                   </div>
@@ -36,8 +53,19 @@ function Inicio() {
               </Card>
             </div>
           </Col>
-        </Row>
-      </div>
+        );
+      }
+
+      // Agregar la fila de cards a la lista de cards
+      cards.push(<Row key={i}>{filaCards}</Row>);
+    }
+
+    return cards;
+  };
+
+  return (
+    <form action="">
+      <div>{renderCards()}</div>
     </form>
   );
 }
--- src/componentsUser/Vistas/Login.jsx
+++ src/componentsUser/Vistas/Login.jsx
@@ -7,16 +7,97 @@ import { useNavigate } from 'react-router';
 import '../css/login.css';
 import axios from 'axios';
 
+async function validarInicio(mail, pass, navigate) {
+  if (mail.trim("") && pass.trim("")) {
+    if (mail.includes("@")) {
+      if (/^\w+([.]\w+)*@\w+([.]\w+)*[.][a-zA-Z]{2,5}$/.test(mail)) {
+        try {
+          const response = await axios.post('http://jeshuabd-001-site1.dtempurl.com/api/Usuarios/VerificarLogin?correo=' + mail + '&contraseña=' + pass);
+          if (response.status === 200) {
+            Swal.fire({
+              icon: 'success',
+              title: 'Todo correcto',
+              text: 'Iniciando sesión...',
+              showConfirmButton: true,
+              confirmButtonText: 'Entrar'
+            }).then(
+              function (result) {
+                if (result.isConfirmed) {
+                  navigate('/PrincipalUser');
+                }
+              }
+            );
+          }
+        } catch (error) {
+          Swal.fire({
+            icon: 'error',
+            title: 'Usuario no encontrado',
+            text: 'Asegúrese de escribir correctamente su usuario y contraseña.',
+            showConfirmButton: false,
+            showDenyButton: true,
+            denyButtonText: 'Volver a intentarlo'
+          });
+        }
+      }
+    }
+    else if (!(/\d/.test(mail))) {
+      try {
+        const response = await axios.post('http://jeshuabd-001-site1.dtempurl.com/api/Usuarios/VerificarLogin?correo=' + mail + '&contraseña=' + pass);
+        if (response.status === 200) {
+          Swal.fire({
+            icon: 'success',
+            title: 'Todo correcto',
+            text: 'Iniciando sesión...',
+            showConfirmButton: true,
+            confirmButtonText: 'Entrar'
+          }).then(
+            function (result) {
+              if (result.isConfirmed) {
+                navigate('/PrincipalUser');
+              }
+            }
+          );
+        }
+      } catch (error) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Usuario no encontrado',
+          text: 'Asegúrese de escribir correctamente su usuario y contraseña.',
+          showConfirmButton: false,
+          showDenyButton: true,
+          denyButtonText: 'Volver a intentarlo'
+        });
+      }
+    }
+
+    else if (/\d/.test(mail)) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Nombre imposible',
+        text: 'Se detecto numeros en su nombre, intente de nuevo',
+        showConfirmButton: false,
+        showDenyButton: true,
+        denyButtonText: 'Volver a intentarlo'
+      });
+    }
+
+  }
+  else {
+    Swal.fire({
+      icon: 'info',
+      title: 'Espacios vacíos',
+      text: 'Rellene los campos de ingreso',
+      showConfirmButton: true,
+      confirmButtonText: 'Volver a intentarlo'
+    })
+  }
+}
+
 function BasicExample() {
   const navigate = useNavigate();
-  const [mail, setMail] = useState("");
-  const [pass, setPass] = useState("");
-  
-  return (
+  const [mail, setMail] = useState('');
+  const [pass, setPass] = useState('');
 
-
-
- 
   return (
     <div className="login-container">
       <div className="login-content">
@@ -27,11 +108,11 @@ function BasicExample() {
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label className="input-label text-center">Ingresa tu correo o usuario</Form.Label>
               <Form.Control
-                 className='email' 
-                 type="email" 
-                 placeholder="correo/usuario"
-                 value={mail}
-                 onChange={ev => setMail(ev.target.value)} 
+                className='email'
+                type="email"
+                placeholder="correo/usuario"
+                value={mail}
+                onChange={ev => setMail(ev.target.value)}
               />
             </Form.Group>
           </div>
@@ -41,179 +122,38 @@ function BasicExample() {
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label className="input-label text-center">Contraseña</Form.Label>
               <Form.Control
-               className='password' 
-               type="password" 
-               placeholder="contraseña"
-               value={pass}
-               onChange={ev => setPass(ev.target.value)} 
+                className='password'
+                type="password"
+                placeholder="contraseña"
+                value={pass}
+                onChange={ev => setPass(ev.target.value)}
               />
             </Form.Group>
           </div>
 
           <Button className="botonInicio" variant="primary" type="submit" onClick={ev => validarInicio(mail, pass, navigate)}>
-        Iniciar sesión <AiOutlineArrowRight className="button-icon" />
-      </Button>
+            Iniciar sesión <AiOutlineArrowRight className="button-icon" />
+          </Button>
 
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Ingresa tu correo ó usuario</Form.Label>
-        <Form.Control 
-          className='email' 
-          type="email" 
-          placeholder="correo/usuario"
-          value={mail}
-          onChange={ev => setMail(ev.target.value)} />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Contraseña</Form.Label>
-        <Form.Control 
-          className='password' 
-          type="password" 
-          placeholder="contraseña"
-          value={pass}
-          onChange={ev => setPass(ev.target.value)} />
-      </Form.Group>
-      
-      <Button 
-        className='botonInicio' 
-        variant="primary" 
-        type="button"
-        onClick={ev => validarInicio(mail, pass, navigate)}>
-        Iniciar sesión
-      </Button>
-      <br /><br /><br /><br />
-      <div>
-        <Row>
-          <Col>
-            <h6 className='letraNoCuenta'>¿No tienes una cuenta? registrate</h6> 
-          </Col>
-          <Col>
-            <Button href='/CrearCuentaUser' className='registrarse' variant="primary" type="button">Registrarse</Button>
-          </Col>
-        </Row>
-        <br />
-        <Row>
-          <Col>
-            <h6 className='letraProv'>¿Desea ingresar como proveedor?</h6> 
-          </Col>
-          <Col>
-            <Button href='/loginProv' className='iniciaProve' variant="primary" type="button">Iniciar sesion</Button>
-          </Col>
-        </Row>
-    </div>
-      
-    </Form>
-    
-      </body>
-   <footer>
-    
-   </footer>
-      <div className="linea-separadora"></div> {/* Línea separadora */}
-          
-      <div className="register-section">
-        <h6 className="letraNoCuenta">¿No tienes una cuenta? Regístrate</h6>
-        <Button href="/CrearCuentaUser" className="registrarse" variant="primary" type="submit">
-          <FaUserPlus className="button-icon" /> Registrarse
-        </Button>
-      </div>
+          <div className="linea-separadora"></div> {/* Línea separadora */}
 
-      <div className="provider-section">
-        <h6 className="letraProv">¿Deseas ingresar como proveedor?</h6>
-        <Button href="/loginProv" className="iniciaProve" variant="primary" type="submit">
-          Iniciar sesión <AiOutlineArrowRight className="button-icon" />
-        </Button>
-      </div>
+          <div className="register-section">
+            <h6 className="letraNoCuenta">¿No tienes una cuenta? Regístrate</h6>
+            <Button href="/CrearCuentaUser" className="registrarse" variant="primary" type="submit">
+              <FaUserPlus className="button-icon" /> Registrarse
+            </Button>
+          </div>
+
+          <div className="provider-section">
+            <h6 className="letraProv">¿Deseas ingresar como proveedor?</h6>
+            <Button href="/loginProv" className="iniciaProve" variant="primary" type="submit">
+              Iniciar sesión <AiOutlineArrowRight className="button-icon" />
+            </Button>
+          </div>
         </Form>
       </div>
     </div>
   );
 }
 
-
-async function validarInicio(mail, pass, navigate){
-  if(mail.trim("") && pass.trim("")){
-    if(mail.includes("@")){
-      if(/^\w+([.]\w+)*@\w+([.]\w+)*[.][a-zA-Z]{2,5}$/.test(mail)){
-        try {
-          const response = await axios.post('http://jeshuabd-001-site1.dtempurl.com/api/Usuarios/VerificarLogin?correo='+mail+'&contraseña='+pass);
-          if (response.status === 200) {
-            Swal.fire({
-              icon:'success',
-              title:'Todo correcto',
-              text:'Iniciando sesión...',
-              showConfirmButton:true,
-              confirmButtonText:'Entrar'
-          }).then(
-              function (result){
-                  if(result.isConfirmed){
-                      navigate('/PrincipalUser');
-                  }
-              }
-            );
-          } 
-        } catch (error) {
-          Swal.fire({
-            icon:'error',
-            title:'Usuario no encontrado',
-            text:'Asegúrese de escribir correctamente su usuario y contraseña.',
-            showConfirmButton:false,
-            showDenyButton:true,
-            denyButtonText:'Volver a intentarlo'
-          });
-        }
-      }
-    }
-    
-    else if(!(/\d/.test(mail))){
-      try {
-        const response = await axios.post('http://jeshuabd-001-site1.dtempurl.com/api/Usuarios/VerificarLogin?correo='+mail+'&contraseña='+pass);
-        if (response.status === 200) {
-          Swal.fire({
-            icon:'success',
-            title:'Todo correcto',
-            text:'Iniciando sesión...',
-            showConfirmButton:true,
-            confirmButtonText:'Entrar'
-        }).then(
-            function (result){
-                if(result.isConfirmed){
-                    navigate('/PrincipalUser');
-                }
-            }
-          );
-        } 
-      } catch (error) {
-        Swal.fire({
-          icon:'error',
-          title:'Usuario no encontrado',
-          text:'Asegúrese de escribir correctamente su usuario y contraseña.',
-          showConfirmButton:false,
-          showDenyButton:true,
-          denyButtonText:'Volver a intentarlo'
-        });
-      }
-    }
-
-    else if(/\d/.test(mail)){
-      Swal.fire({
-        icon:'error',
-        title:'Nombre imposible',
-        text:'Se detecto numeros en su nombre, intente de nuevo',
-        showConfirmButton:false,
-        showDenyButton:true,
-        denyButtonText:'Volver a intentarlo'
-      });
-    }
-  
-  }
-  else{
-    Swal.fire({
-      icon:'info',
-      title:'Espacios vacíos',
-      text:'Rellene los campos de ingreso',
-      showConfirmButton:true,
-      confirmButtonText:'Volver a intentarlo'
-    })
-  }
-}
 export default BasicExample;
-
