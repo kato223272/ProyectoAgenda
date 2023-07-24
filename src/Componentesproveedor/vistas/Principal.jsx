158695e834a2d43ef35b490ea1ca929d41aca34b
kato223272 <223272@ids.upchiapas.edu.mx>
2023-07-23 22:49:21 -0600

    pre-actualización proveedor

--- src/Componentesproveedor/vistas/Principal.jsx
+++ src/Componentesproveedor/vistas/Principal.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from '../components/NavbarProvTodas';
 import Card from '../components/Card';
 import { AiOutlineClockCircle } from 'react-icons/ai';
-import { format, startOfWeek, endOfWeek } from 'date-fns'; 
+import { format, startOfWeek, endOfWeek, isMonday } from 'date-fns';
 import Footer from '../../ComponentGlobales/Footer.jsx';
 import '../css/principal.css';
 
 function Principal() {
   // Datos de disponibilidad simulados (algunas horas ocupadas)
-  const disponibilidad = [
+  const disponibilidadInicial = [
     { dia: 'Lunes', horas: ['08:00', '09:00', '10:00', '13:00'] },
     { dia: 'Martes', horas: ['09:00', '10:00', '11:00'] },
     { dia: 'Miércoles', horas: ['08:00', '09:00', '10:00', '11:00', '12:00', '14:00'] },
@@ -27,11 +27,32 @@ function Principal() {
   const fechaInicioTexto = format(inicioSemana, 'dd/MM/yyyy');
   const fechaFinTexto = format(finSemana, 'dd/MM/yyyy');
 
+  const [disponibilidad, setDisponibilidad] = useState(disponibilidadInicial);
+
+  useEffect(() => {
+    // Verificar si es lunes para actualizar la disponibilidad
+    if (isMonday(fechaActual)) {
+      // Datos de disponibilidad actualizados para una nueva semana (algunas horas ocupadas)
+      const nuevaDisponibilidad = [
+        { dia: 'Lunes', horas: ['08:00', '09:00', '10:00', '11:00', '12:00'] },
+        { dia: 'Martes', horas: ['09:00', '10:00', '11:00', '12:00'] },
+        { dia: 'Miércoles', horas: ['08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00'] },
+        { dia: 'Jueves', horas: ['08:00', '09:00', '10:00', '11:00'] },
+        { dia: 'Viernes', horas: ['10:00', '11:00', '12:00', '13:00', '14:00'] },
+      ];
+
+      setDisponibilidad(nuevaDisponibilidad);
+    }
+  }, [fechaActual]);
+
   return (
     <div>
       <Navbar />
       <br />
       <Card />
+      <br />
+      <div className="division"></div>
+      <br />
       <div className="horario-header">
         <h5 className="LetraHorario">
           <AiOutlineClockCircle className="icono-horario" /> Horario disponibilidad
@@ -42,6 +63,7 @@ function Principal() {
           </p>
         </div>
       </div>
+      
       <div className='horario-table'>
         <table>
           <thead>
@@ -71,7 +93,6 @@ function Principal() {
             ))}
           </tbody>
         </table>
-        
       </div>
       <br></br><br></br>
       <Footer></Footer>
